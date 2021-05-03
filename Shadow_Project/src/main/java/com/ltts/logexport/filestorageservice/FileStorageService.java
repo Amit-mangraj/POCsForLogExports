6ef7b23b758c0dabcb49013e29d02d672c899926
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-05-03 18:23:37 +0530

    Added Service

--- Shadow_Project/src/main/java/com/ltts/logexport/filestorageservice/FileStorageService.java
+++ Shadow_Project/src/main/java/com/ltts/logexport/filestorageservice/FileStorageService.java
@@ -1,5 +1,25 @@
 package com.ltts.logexport.filestorageservice;
 
 public class FileStorageService {
-
+   public static void storage(HttpServletResponse res) throws IOException, Exception
+	{
+		ConvertToZip.zipFile();
+		System.out.println("File zipped");
+		File file = new File(Properties.zipFilepath);
+		res.setContentType("application/octet-stream");
+		String headerKey ="Content-Disposition";
+		String headerValue="attachment;filename="+file.getName();
+		res.setHeader(headerKey, headerValue);
+		ServletOutputStream outputStream = res.getOutputStream();
+		BufferedInputStream inputStream =new BufferedInputStream(new FileInputStream(file));
+		byte[] buffer = new byte[8192];
+		int bytesRead=-1;
+		while((bytesRead=inputStream.read(buffer))!=-1)
+		{
+			outputStream.write(buffer,0,bytesRead);
+			
+		}
+		inputStream.close();
+		outputStream.close();
+	}
 }
