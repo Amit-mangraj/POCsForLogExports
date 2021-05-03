febf7e6dc78ff4f9ec40fb21fe828ddc43afc082
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-05-03 18:20:19 +0530

    Added Exception

--- Shadow_Project/src/main/java/com/ltts/logexport/exception/logException.java
+++ Shadow_Project/src/main/java/com/ltts/logexport/exception/logException.java
@@ -1,5 +1,9 @@
 package com.ltts.logexport.exception;
 
-public class logException {
-
+public class logException extends public logException()
+{
+  public logException()
+	{
+		System.out.println("Logs Not Found"); //user defined exception
+	}
 }
