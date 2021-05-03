ea5bad19151de6f3326affa9ef8adaa06a143ce7
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-05-03 19:52:21 +0530

    Added Service

--- Shadow_Project/src/main/java/com/ltts/logexport/controller/HomeController.java
+++ Shadow_Project/src/main/java/com/ltts/logexport/controller/HomeController.java
@@ -1,48 +1,30 @@
-package com.ltts1.Shadow.Controller;
+package com.ltts.logexport.controller;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.ltts1.Shadow.Zip.ZipFile;
+import org.springframework.web.servlet.ModelAndView;
+import com.ltts.logexport.exception.logException;
+import com.ltts.logexport.filestorageservice.FileStorageService;
 
 @Controller
 public class HomeController {
-
 	@GetMapping("")
 	public String view() {
+		return "home";
+	}
 
-		return "index";
-		
-	}
 	@GetMapping("/download")
-	public void download(HttpServletResponse res)throws IOException
-	{
-		ZipFile.zip_one_file();
-		System.out.println("File zipped");
-		File file = new File("D:\\log\\spring.zip");
-		res.setContentType("application/octet-stream");
-		String headerKey ="Content-Disposition";
-		String headerValue="attachment;filename="+file.getName();
-		res.setHeader(headerKey, headerValue);
-		ServletOutputStream outputStream = res.getOutputStream();
-		BufferedInputStream inputStream =new BufferedInputStream(new FileInputStream(file));
-		byte[] buffer = new byte[8192];
-		int bytesRead=-1;
-		while((bytesRead=inputStream.read(buffer))!=-1)
-		{
-			outputStream.write(buffer,0,bytesRead);
-			
+	public ModelAndView download(HttpServletResponse res) throws Exception {
+		ModelAndView mv = null;
+		try {
+			FileStorageService.storage(res);
+			mv = new ModelAndView("home");
+		} catch (Exception ex) {
+			mv = new ModelAndView("error");
+			throw new logException();
 		}
-		inputStream.close();
-		outputStream.close();
+		return mv;
 	}
-	
+
 }
