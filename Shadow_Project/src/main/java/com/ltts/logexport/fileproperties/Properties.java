6050b61187488b69a6da77bab4ca05dec4c12100
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-05-03 19:37:15 +0530

    Added Properties

--- Shadow_Project/src/main/java/com/ltts/logexport/fileproperties/Properties.java
+++ Shadow_Project/src/main/java/com/ltts/logexport/fileproperties/Properties.java
@@ -1,5 +1,7 @@
 package com.ltts.logexport.fileproperties;
 
 public class Properties {
-
+  public static String zipFilepath = "D:\\log\\spring.zip";
+	public static String logFilepath = "D:\\log\\spring.log"
+	public static String logFileName = "spring1.log";
 }
