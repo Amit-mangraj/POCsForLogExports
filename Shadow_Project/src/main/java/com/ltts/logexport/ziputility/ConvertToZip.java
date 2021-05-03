406ac8eecf147d8f9c912e66d60260e169c5e7db
P99003673 <78849897+P99003673@users.noreply.github.com>
2021-05-03 17:18:02 +0530

    Added Zip Functionality

--- Shadow_Project/src/main/java/com/ltts/logexport/ziputility/ConvertToZip.java
+++ Shadow_Project/src/main/java/com/ltts/logexport/ziputility/ConvertToZip.java
@@ -1,5 +1,24 @@
 package com.ltts.logexport.ziputility;
 
 public class ConvertToZip {
-
+  public static void zipFile() throws IOException, Exception {
+    	byte[] buffer = new byte[1024];
+    	try{
+    	FileOutputStream fileObject = new FileOutputStream(Properties.zipFilepath);
+        ZipOutputStream zipObject = new ZipOutputStream(fileObject);
+    	ZipEntry ze= new ZipEntry(Properties.logFileName);
+    	zipObject.putNextEntry(ze);
+    	FileInputStream in = new FileInputStream(Properties.logFilepath);
+    	System.out.println("input stream "  + in);
+    	int len;
+    	while ((len = in.read(buffer)) > 0) {
+    		zipObject.write(buffer, 0, len);
+    	}
+    	in.close();
+    	zipObject.closeEntry();
+    	zipObject.close();
+    	}catch(Exception ex){
+    		throw new logException();
+    	}
+	}
 }
