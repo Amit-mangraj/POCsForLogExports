c3ba57e91b70a7d8bb3194c20b6f9d71d4af0945
Pavitra Katiyar <78842197+99003685@users.noreply.github.com>
2021-05-06 11:20:28 +0530

    Delete Shadow_Project directory
    Ruko zara

--- Shadow_Project/src/main/java/com/ltts/logexport/exception/logException.java
+++
@@ -1,9 +0,0 @@
-package com.ltts.logexport.exception;
-
-public class logException extends Exception
-{
-  public logException()
-	{
-		System.out.println("Logs Not Found"); //user defined exception
-	}
-}
