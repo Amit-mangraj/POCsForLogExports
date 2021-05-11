2241dfd3c0f9232f4e80b584007ac3807656551b
99003685 <pavitra.katiyar@ltts.com>
2021-05-11 11:51:58 +0530

    Some Changes

---
+++ POCForLogExport/src/main/java/com/ltts/logexport/PocForLogExportApplication.java
@@ -0,0 +1,13 @@
+package com.ltts.logexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocForLogExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocForLogExportApplication.class, args);
+	}
+
+}
---
+++ POCForLogExport/src/test/java/com/ltts/logexport/PocForLogExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.logexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocForLogExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
